db26028f8729c3e3c504e6cdf67e78966b2e832f
Manoj <stdiomozz313@gmail.com>
2024-09-17 11:51:43 +0530

    exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 
 const Statistics = ({good,neutral,bad}) => {
+
+  if(good===0 && neutral===0 && bad===0){
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </>
+    )
+  }
   return (
     <>
     <h1>statistics</h1>
@@ -19,7 +28,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total,setTotal] = useState(0)
 
   return (
     <div>
